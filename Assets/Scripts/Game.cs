c5b5bdcbcc5b3232c4b2000b3d184a8227dfa979
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2019-02-15 15:07:07 +0800

    对象池

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -5,6 +5,21 @@ using UnityEngine;
 
 public class Game : PersistableObject
 {
+    private static Game instance;
+
+    public static Game Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = GameObject.Find("Game").GetComponent<Game>();
+            }
+
+            return instance;
+        }
+    }
+
     public const int nowSaveVersion = 3;
     public const int version_1 = 1; //版本1储存的是shape的shapeId
     public const int version_2 = 2; //版本2储存的是shape的materialId
@@ -14,11 +29,17 @@ public class Game : PersistableObject
     public PersistenStorage storage;
 
     public KeyCode createKey = KeyCode.C;
+    public KeyCode destoryKey = KeyCode.X;
     public KeyCode newGameKey = KeyCode.N;
     public KeyCode saveKey = KeyCode.S;
     public KeyCode loadKey = KeyCode.L;
 
     private List<Shape> shapes;
+
+    private float creationSpeed;
+    private float creationProgress;
+    private float destructionSpeed;
+    private float destructionProgress;
 
     private void Awake()
     {
@@ -30,6 +51,10 @@ public class Game : PersistableObject
         if (Input.GetKeyDown(createKey))
         {
             CreateShape();
+        }
+        else if (Input.GetKeyDown(destoryKey))
+        {
+            DestroyShape();
         }
         else if (Input.GetKeyDown(newGameKey))
         {
@@ -44,6 +69,30 @@ public class Game : PersistableObject
             BeginNewGame();
             storage.Load(this);
         }
+
+        creationProgress += Time.deltaTime * creationSpeed;
+        while (creationProgress >= 1)
+        {
+            creationProgress -= 1;
+            CreateShape();
+        }
+
+        destructionProgress += Time.deltaTime * destructionSpeed;
+        while (destructionProgress >= 1)
+        {
+            destructionProgress -= 1;
+            DestroyShape();
+        }
+    }
+
+    public void ChangeCreationSpeed(float val)
+    {
+        creationSpeed = val;
+    }
+
+    public void ChangeDestructionSpeed(float val)
+    {
+        destructionSpeed = val;
     }
 
 
@@ -62,11 +111,24 @@ public class Game : PersistableObject
         shapes.Add(instance);
     }
 
+    private void DestroyShape()
+    {
+        if (shapes.Count > 0)
+        {
+            int index = Random.Range(0, shapes.Count);
+            shapeFacotry.Reclaim(shapes[index]);
+            int lastIndex = shapes.Count - 1;
+            shapes[index] = shapes[lastIndex];
+            shapes.RemoveAt(lastIndex);
+        }
+
+    }
+
     private void BeginNewGame()
     {
         foreach (var obj in shapes)
         {
-            Destroy(obj.gameObject);
+            shapeFacotry.Reclaim(obj);
         }
 
         shapes.Clear();
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    private static UIManager instance;
+
+    public static UIManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = GameObject.Find("UIRoot").GetComponent<UIManager>();
+            }
+
+            return instance;
+        }
+    }
+
+
+    private void Awake()
+    {
+        Transform root = transform;
+        Slider creationSpeedSlider = root.Find("CreationSpeedSlider").GetComponent<Slider>();
+        Slider destructionSpeedSlider = root.Find("DestructionSpeedSlider").GetComponent<Slider>();
+
+        creationSpeedSlider.onValueChanged.AddListener(Game.Instance.ChangeCreationSpeed);
+        destructionSpeedSlider.onValueChanged.AddListener(Game.Instance.ChangeDestructionSpeed);
+    }
+}
