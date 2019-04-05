e497c6d3a2b8ce4df8b5ffdd086c5c16b5c44769
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2019-04-05 14:21:47 +0800

    Slider

--- Assets/Scripts/ColorRangeHSV.cs
+++ Assets/Scripts/ColorRangeHSV.cs
@@ -3,19 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-public struct ColorRangeHSV 
+public struct ColorRangeHSV
 {
+    [FloatRangeSlider(0f, 1f)]
     public FloatRange hue, saturation, value;
 
-    public Color RandomInRange
-    {
-        get
-        {
-            return Random.ColorHSV(
-                hue.min, hue.max,
-                saturation.min, saturation.max,
-                value.min, value.min,
-                1f, 1f);
-        }
-    }
+    public Color RandomInRange => Random.ColorHSV(
+        hue.min, hue.max,
+        saturation.min, saturation.max,
+        value.min, value.min,
+        1f, 1f);
 }
---
+++ Assets/Scripts/FloatRangeSliderAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatRangeSliderAttribute : PropertyAttribute
+{
+    public float Min { get; set; }
+    public float Max { get; set; }
+
+    public FloatRangeSliderAttribute(float min, float max)
+    {
+        if (max < min)
+        {
+            Max = min;
+            Min = max;
+        }
+        else
+        {
+            Min = min;
+            Max = max;
+        }
+    }
+}
